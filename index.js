8a540575569f6750890cef50fc5b784a51f5364d
Kaitlyn Hoyme <khoyme@iastate.edu>
2024-10-17 18:22:46 -0500

    added second div and carousel beginnings

--- index.js
+++ index.js
@@ -33,3 +33,58 @@ window.addEventListener('resize', () => {
         profileText.classList.remove('d-none');
     }
 });
+
+
+// load the images and texts for the homepage
+
+fetchData();
+function fetchData(){
+    //read the form
+    const b = document.getElementById("carousel-inner-group");
+    // b.addEventListener("submit",(event)=>{
+    // event.preventDefault(); // Prevent the form from submitting in the traditional way
+    fetch('./home.json')
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        loadCarouselImages(data.homeimages);
+    })
+    .catch(function (err) {
+        console.log('error:' + err);
+    });
+
+    // });
+}
+
+
+function loadCarouselImages(data){
+
+    var carouselGroup = document.getElementById("carousel-inner-group");
+    for (let i = 0; i < data.length; i++) {
+        console.log(data);
+        let label = data[i].label;
+        let path = data[i].path;
+        let description = data[i].description;
+        let alt = data[i].alt;
+        let imageId = data[i].imageId;
+        // construct the HTML element
+        let AddCarouselItem = document.createElement("div");
+        if (i===0){
+            AddCarouselItem.classList.add("active");
+        }
+
+        AddCarouselItem.classList.add("carousel-item"); // Add Bootstrap class to the column
+        AddCarouselItem.classList.add("mx-auto");
+        AddCarouselItem.innerHTML = `
+            <img src=${path} class="d-block w-100" alt=${alt}>
+                <div class="carousel-caption d-none d-md-block">
+                <h5>${label}</h5>
+                <p>${description}</p>
+                </div>
+            `;
+        carouselGroup.appendChild(AddCarouselItem);
+    } 
+} 
+
