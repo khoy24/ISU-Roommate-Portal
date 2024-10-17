d09d36c260f869b8f411f0ba645769f7c7417af8
moseleyc@iastate.edu <glpat-MXzM5yMcxi57C6j-2CR4>
2024-10-17 18:34:29 -0500

    added housing details for all dorms, need to fix card alignment when expanded for description

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let profileText = document.getElementById('p-text');
 
 // checks to change profile vs icon when clicked
 navbarToggler.addEventListener('click', () => {
-
     // keep track of the clicks of the toggle to see whether it is expanded or not
     if (navbarToggler.getAttribute('aria-expanded') === "true") {
         console.log("expanded");
@@ -33,3 +32,68 @@ window.addEventListener('resize', () => {
         profileText.classList.remove('d-none');
     }
 });
+
+function getHalls() {
+    fetch("./data.json")
+    .then(response => response.json())
+    .then(halls => loadImages(halls, 1))
+    .catch(err => console.log("error: " + err))
+    // console.log(halls);
+}
+
+function loadImages(Images, n) {
+    const imageArray = [];
+
+    //if loading residence halls
+    if (n === 1) {
+    Images.Halls.forEach(hall => {
+        imageArray.push(
+            hall
+        )
+    });
+    }
+
+    console.log(imageArray);
+
+    var Card = document.getElementById("col");
+
+    for (var i = 0; i < imageArray.length; i++) {
+        let name = imageArray[i].Name;
+        let Price = imageArray[i].Price;
+        let AirConditioned = imageArray[i].AirConditioned;
+        let OpenDuringWinterBreak = imageArray[i].OpenDuringWinterBreak;
+        let Eligibility = imageArray[i].Eligibility;
+        let URL = imageArray[i].URL;
+        let description = imageArray[i].Description;
+        let AddCard = document.createElement("div");
+        AddCard.classList.add("col");
+        AddCard.innerHTML = `
+        <div class="card shadow-sm">
+        <img src=${URL} class="card-img-top" alt="..."></img>
+        <div class="card-body">
+        <p class="card-text"> <strong>${name}</strong>, $${Price} <br>
+        Air Conditioned: ${AirConditioned}<br>
+        Open for Winter Break: ${OpenDuringWinterBreak} <br>
+        <button id="${name}">button</button> 
+        <p style="display: none;">${description}</p>
+        </div>
+        </div>
+        `;
+        Card.appendChild(AddCard);
+    }
+}
+
+if ($('body').is('.housing')){
+    let temp = document.querySelectorAll('.button');
+
+    temp.forEach((item) => {
+        item.addEventListener('click', () => {
+            console.log(item.id);
+        })
+    });
+}
+
+/* <details>
+<summary style="display: block; width: 87px; padding: 2px; list-style: none; border-style: solid; border-radius: 4px">Description</summary>
+<p>${description}</p>
+</details> */
